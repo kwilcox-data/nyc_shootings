9ea3f0a1cf309ddae6d67dea314e28d745c9c953
kwilcox-data <51834141+kwilcox-data@users.noreply.github.com>
2022-12-02 19:50:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
 
   var TITLE = 'Income and Test Scores in Connecticut School Districts, 2009-13';
 
-  var POINT_X = 'income'; // column name for x values in data.csv
-  var POINT_X_PREFIX = '$'; // prefix for x values, eg '$'
+  var POINT_X = 'OCCUR_DATE	'; // column name for x values in data.csv
+  var POINT_X_PREFIX = ''; // prefix for x values, eg '$'
   var POINT_X_POSTFIX = ''; // postfix for x values, eg '%'
 
-  var POINT_Y = 'grades'; // column name for y values in data.csv
+  var POINT_Y = 'OCCUR_TIME'; // column name for y values in data.csv
   var POINT_Y_PREFIX = ''; // prefix for x values, eg 'USD '
   var POINT_Y_POSTFIX = ''; // postfix for x values, eg ' kg'
 
-  var POINT_NAME = 'district'; // point names that appear in tooltip
+  var POINT_NAME = 'VIC_AGE_GROUP,VIC_SEX,VIC_RACE'; // point names that appear in tooltip
   var POINT_COLOR = 'rgba(0,0,255,0.7)'; // eg `black` or `rgba(10,100,44,0.8)`
   var POINT_RADIUS = 5; // radius of each data point
 
-  var X_AXIS = 'Median Household Income, USD'; // x-axis label, label in tooltip
-  var Y_AXIS = 'Grade, Relative to Average'; // y-axis label, label in tooltip
+  var X_AXIS = 'Date of Shooting'; // x-axis label, label in tooltip
+  var Y_AXIS = 'Time of Shooting'; // y-axis label, label in tooltip
 
   var SHOW_GRID = true; // `true` to show the grid, `false` to hide
 
